87821eedd506b1a6de455a7916794294409a087f
Adrian Firmalan <afirmalan@yahoo.com>
2022-08-17 15:57:56 -0700

    routes for story posting

---
+++ Develop/controllers/api/newStoryRoutes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const { Story } = require ('../../models');
+const { continueStory } = require ('../../models');
+
+// route for publishing new story
+
+router.post('/', async (req, res) = {
+    try {
+        const newStory = await Story.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status (200).json(newStory);
+    } catch (err) {
+        res.status(400).json(err)}
+
+});
+
+// add comment/ continue the story to add on to the story
+router.update("/continue", aysnc(req,res) = {
+    try {
+        const continueStory = await Story.create({
+
+        })
+    }
+})
+
+// delete story with story id
+
+router.delete('/:id', aysnc (req, res) => {
+    try {
+        const storyData = await Story.destroy({
+            where: {
+                id: req.parasm.id,
+                user_id: req.session.user_id,
+            },
+        });
+        
+        if (!storyData) {
+            res.status(404).json({message: 'No story found with this id!'});
+            return;
+        }
+        res.status(200).json(storyData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const User = require('//models//')
 
+// the sign in information
+
 router.post('/', async (req, res) => {
     try {
         const userData = await User.create(req.body);
@@ -16,6 +18,8 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+// log in route
 
 router.post('/login', async (req, res) => {
     try {
@@ -39,6 +43,8 @@ router.post('/login', async (req, res) => {
 
     }
 });
+
+// path for log out
 
 router.post('/logout', (req, res) => {
     if (req.session.logged_in) {
